48db3ad6297c2bd6d0f6c35285441734447d4e55
Rayuga <adityapatkar81@gmail.com>
2024-07-27 16:29:52 +0530

    r5x: correct bootanimation res

--- voltage_r5x.mk
+++ voltage_r5x.mk
@@ -8,8 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelOS stuff
+# Inherit some common voltageOS stuff
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1280
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
